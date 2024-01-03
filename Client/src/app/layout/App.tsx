00b696196977439daa9469222740ac84e5861302
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2024-01-03 11:00:30 +0200

    Started client CRUD, implemented list of activities.

---
+++ Client/src/app/layout/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+import axios from "axios";
+import { Container } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/dashboard/ActivityDashboard";
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard activities={activities} />
+      </Container>
+    </>
+  );
+}
+
+export default App;
