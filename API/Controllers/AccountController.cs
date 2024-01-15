6d3c2242a169302965d5c39f4c3bb5f3e6964032
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2024-01-15 09:23:36 +0200

    Clientside login/register

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -56,12 +56,14 @@ namespace API.Controllers
         {
             if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
             {
-                return BadRequest("Username already taken");
+                ModelState.AddModelError("username", "Username taken");
+                return ValidationProblem(ModelState);
             }
 
             if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
             {
-                return BadRequest("Email already taken");
+                ModelState.AddModelError("email", "Email taken");
+                return ValidationProblem(ModelState);
             }
 
             var user = new AppUser
