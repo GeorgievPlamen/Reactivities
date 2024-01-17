c39433fc096a97c101ebb384ed4c7eace216402c
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2024-01-17 10:51:18 +0200

    Added chat functions clientside.

--- Client/src/app/stores/activityStore.ts
+++ Client/src/app/stores/activityStore.ts
@@ -185,6 +185,10 @@ export default class ActivityStore {
             runInAction(() => this.loading = false);
         }
     }
+
+    clearSelectedActivity = () => {
+        this.selectedActivity = undefined;
+    }
 }
 
     
